fc58d8e6e8f18d3f51e1267f074376efee980aac
GiulioMiniDev <65420559+GiulioMiniDev@users.noreply.github.com>
2022-05-01 22:11:44 +0100

    Aggiungere i file di progetto.

---
+++ GDatabaseHandler.cs
@@ -0,0 +1,38 @@
+﻿//using SQLite;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace GUtility
+//{
+//    public class DatabaseHandler<T> where T : new()
+//    {
+//        public ObservableCollection<T> observable { set; get; }
+//        public SQLiteAsyncConnection connection { get; set; }
+//        public DatabaseHandler(string databasePath)
+//        {
+//            //observable = new ObservableCollection<T>();
+//            connection = new SQLiteAsyncConnection(databasePath);
+//        }
+//        public async Task GetAllData()
+//        {
+//            var query = connection.Table<T>();
+//            observable = new ObservableCollection<T>(await query.ToListAsync());
+//        }
+//        public async Task DeleteData(object pk)
+//        {
+//            await connection.DeleteAsync<T>(pk);
+//        }
+//        public async Task InsertOneElement(T element)
+//        {
+//            await connection.InsertAsync(element);
+//        }
+//        public async Task CreateTable()
+//        {
+//            await connection.CreateTableAsync<T>();
+//        }
+//    }
+
+//}
